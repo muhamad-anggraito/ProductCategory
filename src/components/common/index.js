74aa9a593f0b67ed1d8557dfbaef9d3fbb9021b3
muhamad-anggraito <muhamad.anggraito@gmail.com>
2020-10-04 14:44:37 +0700

    checkpoint 2

---
+++ src/components/common/index.js
@@ -0,0 +1,8 @@
+import Button from './Button';
+import Card from './Card';
+import CardSection from './CardSection';
+import Header from './Header';
+import Input from './Input';
+import Spinner from './Spinner';
+
+export {Button, Card, CardSection, Header, Input, Spinner};
